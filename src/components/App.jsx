fd1e238368b0ee19e203979feb2494d15b8f71fe
Yevhen Vasylevytskyi <e.vasylevytskyi@gmail.com>
2023-03-29 21:47:09 +0300

    add Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,18 @@ import FriendList from './FriendList/FriendList';
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
 import TransactionHistory from './TransactionHistory/TransactionHistory';
+import user from '../data/user.json';
 
 export const App = () => {
   return (
     <>
-      <Profile />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
       <Statistics />
       <FriendList />
       <TransactionHistory />
