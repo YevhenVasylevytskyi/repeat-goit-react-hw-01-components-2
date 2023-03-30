0b2d05f39ec62d5eab0db5c855ecff20a80101cc
Yevhen Vasylevytskyi <e.vasylevytskyi@gmail.com>
2023-03-30 08:44:53 +0300

    add PropTypes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-const Profile = ({username, tag, location, avatar, stats}) => {
+
+const Profile = ({
+  username = 'John Doe',
+  tag = '@jdoe',
+  location = 'Unknown',
+  avatar = "https://cdn-icons-png.flaticon.com/512/1077/1077012.png",
+  stats: {
+    followers = 0,
+    views = 0,
+    likes = 0,
+  }
+}) => {
 
   return (
     <div className="profile">
@@ -18,19 +30,31 @@ const Profile = ({username, tag, location, avatar, stats}) => {
   <ul className="stats">
     <li>
       <span className="label">Followers</span>
-      <span className="quantity">{stats.followers}</span>
+      <span className="quantity">{followers}</span>
     </li>
     <li>
       <span className="label">Views</span>
-      <span className="quantity">{stats.views}</span>
+      <span className="quantity">{views}</span>
     </li>
     <li>
       <span className="label">Likes</span>
-      <span className="quantity">{stats.likes}</span>
+      <span className="quantity">{likes}</span>
     </li>
   </ul>
 </div>
   );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.exact({
+    followers: PropTypes.number,
+    views: PropTypes.number,
+    likes: PropTypes.number,
+  }),
 };
 
 export default Profile;
